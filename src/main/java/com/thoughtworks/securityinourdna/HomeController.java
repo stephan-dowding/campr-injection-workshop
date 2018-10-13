8d9b2b6594cdc7f8167edee0ba045a17eddff755
Stephan Dowding <sdowding@thoughtworks.com>
2018-10-13 19:25:29 +0800

    regular form post

--- src/main/java/com/thoughtworks/securityinourdna/HomeController.java
+++ src/main/java/com/thoughtworks/securityinourdna/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.SQLException;
 
@@ -21,5 +23,17 @@ public class HomeController {
     public String home(Model model) throws SQLException {
         model.addAttribute("vendors", userRepo.allVendors());
         return "home";
+    }
+
+    @PostMapping("/login")
+    public String login(Model model, @RequestParam("vendor") String vendor, @RequestParam("password") String password) throws Exception {
+        final UserRepo.LoginResult loginResult = userRepo.login(vendor, password);
+
+        if (loginResult.success) {
+            model.addAttribute("vendor", loginResult.vendorName);
+            return "login";
+        } else {
+            return "loginFail";
+        }
     }
 }

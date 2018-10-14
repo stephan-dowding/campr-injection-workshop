ef2937e302c184c4409b40c048385af157d6f39a
Stephan Dowding <sdowding@thoughtworks.com>
2018-10-14 22:14:25 +0800

    post redirect get with flash messages

--- src/main/java/com/thoughtworks/securityinourdna/HomeController.java
+++ src/main/java/com/thoughtworks/securityinourdna/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.SQLException;
 
@@ -25,15 +26,23 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/vendor")
+    public String vendor() {
+        return "vendor";
+    }
+
     @PostMapping("/login")
-    public String login(Model model, @RequestParam("vendor") String vendor, @RequestParam("password") String password) throws Exception {
+    public String login(RedirectAttributes redirAttrs,
+                        @RequestParam("vendor") String vendor,
+                        @RequestParam("password") String password) throws Exception {
         final UserRepo.LoginResult loginResult = userRepo.login(vendor, password);
 
         if (loginResult.success) {
-            model.addAttribute("vendor", loginResult.vendorName);
-            return "login";
+            redirAttrs.addFlashAttribute("vendor", loginResult.vendorName);
+            return "redirect:/vendor";
         } else {
-            return "loginFail";
+            redirAttrs.addFlashAttribute("error", "Bad username and password combination");
+            return "redirect:/";
         }
     }
 }
